b146f5c80229e0b8f9361b5515dd55e43ff09d01
Wayne.MJ <150761502+wayne-mj@users.noreply.github.com>
2025-08-05 20:10:42 +1000

    Added testing

--- test/check.f90
+++ test/check.f90
@@ -1,5 +1,36 @@
-program check
-implicit none
+program test_ftoc
+    use VerySimpleTestFramework
+    use ftoc
 
-print *, "Put some tests in here!"
-end program check
+    implicit none
+    
+    call run_tests()
+
+contains
+    subroutine run_tests()
+        real :: t, tol
+        t = 0.
+        tol = 1. * 10.**-2
+
+        call suite("Temp Converter")
+        call test("C to F 0 Tolerance 0.01")
+        call assert_equals(c_to_f(t), 32., tol)
+
+        call test("F to C 0  Tolerance 0.01")
+        call assert_equals(f_to_c(t), -17.78, tol)
+
+        call test("C to K 0  Tolerance 0.01")
+        call assert_equals(c_to_k(t), 273.15, tol)
+
+        call test("F to K 0  Tolerance 0.01")
+        call assert_equals(f_to_k(t), 255.37, tol)
+
+        call test("K to C 0  Tolerance 0.01")
+        call assert_equals(k_to_c(t), -273.15, tol)
+
+        call test("K to F 0  Tolerance 0.01")
+        call assert_equals(k_to_f(t), -459.67, tol)
+
+        call results()
+    end subroutine
+end program test_ftoc
